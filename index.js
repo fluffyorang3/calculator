882eb11cedcd54518f62c1f0a126cbf5aec5bec8
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-31 20:25:18 +0200

    Complete calculator CSS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let num1 = 3;
-let num2 = 4;
+let num1;
+let num2;
 let operation;
 
 const add = function(num1, num2) {
@@ -17,3 +17,16 @@ const divide = function(num1, num2) {
   return num1 / num2;
 }
 
+function operate(num1, num2, operation) {
+  if(operation == "+") {
+    return add(num1, num2);
+  } else if(operation == "-") {
+    return subtract(num1, num2);
+  } else if(operation == "*") {
+    return multiply(num1, num2);
+  } else if(operation == "/") {
+    return divide(num1, num2);
+  } else {
+    return "ERROR";
+  }
+}
