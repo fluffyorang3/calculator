ab31fe315411602938fb59eaaeb41908f31c9557
fluffyorang3 <fluffyorang3@gmail.com>
2024-05-31 17:55:11 +0200

    Write operation functions

---
+++ index.js
@@ -0,0 +1,19 @@
+let num1 = 3;
+let num2 = 4;
+let operation;
+
+const add = function(num1, num2) {
+  return num1 + num2;
+}
+
+const subtract = function(num1, num2) {
+  return num1 - num2;
+}
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+}
+const divide = function(num1, num2) {
+  return num1 / num2;
+}
+
